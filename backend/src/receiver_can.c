7fee035d31bcbca50ed65be6b50eabecc5528b8d
Giuseppe Cucinotta <electricgamerspro@gmail.com>
2026-05-31 18:51:16 +0200

    now using correct socket protocol (CAN_RAW); added binding with if name

--- backend/src/receiver_can.c
+++ backend/src/receiver_can.c
@@ -10,19 +10,45 @@
 #include <pthread.h>
 #include <sys/socket.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// Bind the socekt to the vcan0
+#include <net/if.h>
 
+/* @brief Create an AF_CAN socket and binds it to a vcan0 if
+ * @return can socket file descriptor */
+// TODO: parametric can if name
 static int init_can_socket() {
-  int fd = socket(AF_CAN, SOCK_STREAM, 0);
+  int fd = socket(AF_CAN, SOCK_RAW, CAN_RAW);
 
   if (fd < 0) {
     perror("Socket not created; error occured.\n");
     exit(1);
   }
 
+  unsigned int can_if_index = if_nametoindex("vcan0");
+  if (can_if_index == 0) {
+    perror("[!] Unable to get CAN interface index.\n");
+    exit(1);
+  }
+
+  struct sockaddr_can addr_can = {
+      .can_family = AF_CAN,
+      .can_ifindex = can_if_index,
+  };
+
+  if (bind(fd, (struct sockaddr *)&addr_can, sizeof(addr_can)) < 0) {
+    perror("[!] Can't bind the socket.\n");
+    exit(1);
+  }
+
   return fd;
 }
 
+/* @brief Reads a can_frame from sock_can_fd
+ * @param[in] can socket file descriptor
+ * @param[out] structure where it's saved the frame
+ * @return 0 in case of no errors, -1 in case of a read error and -2 in case of
+ * an incomplete frame
+ * */
 static int get_can_frame(int socket_fd, struct can_frame *cf) {
   int bytes_received = read(socket_fd, cf, sizeof(struct can_frame));
 
@@ -34,11 +60,9 @@ static int get_can_frame(int socket_fd, struct can_frame *cf) {
   return 0;
 }
 
-// TODO: add the void struct frame buffer
 void *receiver_loop(void *b) {
+  raw_frames_buffer *ring_buffer = (raw_frames_buffer *)b;
   int can_fd = init_can_socket();
-
-  raw_frames_buffer *buffer = (raw_frames_buffer *)b;
 
   while (1) {
     struct can_frame cf;
@@ -53,9 +77,7 @@ void *receiver_loop(void *b) {
       continue;
     }
 
-    pthread_mutex_lock(&mutex);
-    // TODO: condition variables !
-    frame_put(buffer, cf);
-    pthread_mutex_unlock(&mutex);
+    // Put frame into the buffer
+    frame_put(ring_buffer, cf);
   }
 }
