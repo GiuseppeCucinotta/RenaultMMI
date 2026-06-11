06c9051040b50bf97e1535bf8e8f63d2919413f2
Giuseppe Cucinotta <125346983+GiuseppeCucinotta@users.noreply.github.com>
2026-06-11 22:35:42 +0200

    matching latest update

--- backend/src/main.c
+++ backend/src/main.c
@@ -1,13 +1,22 @@
 #include <bits/pthreadtypes.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "can_decoder.h"
 #include "receiver_can.h"
 #include "ring_buffer.h"
+#include "udp_sender.h"
 
 #define WORKERS 4
+
+volatile sig_atomic_t keep_running = 1;
+
+void handle_sigint(int sig) {
+  (void)sig;
+  keep_running = 0;
+}
 
 void print_intro() {
   puts(" ____                        _ _   __  __ __  __ ___ ");
@@ -20,12 +29,18 @@ void print_intro() {
 }
 
 int main(void) {
+  struct sigaction sa;
+  sa.sa_handler = handle_sigint;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &sa, NULL);
+
   print_intro();
   printf("[*] Initializing system...\n");
 
   raw_frames_buffer *ring_buffer = buf_init();
 
-  pthread_t receiver_thread;
+  pthread_t receiver_thread, udp_sender;
 
   if (pthread_create(&receiver_thread, NULL, receiver_loop, ring_buffer) < 0) {
     perror("[!] Unable to create the receiver thread.\n");
@@ -39,18 +54,35 @@ int main(void) {
   pthread_t decoders[WORKERS];
   for (int i = 0; i < WORKERS; i++) {
     if (pthread_create(&decoders[i], NULL, decoder_loop, ring_buffer) < 0) {
-      perror("[!] Unable to create %d decoder thread.\n");
+      perror("[!] Unable to create decoder thread.\n");
       return 1;
     }
   }
 
-  pthread_join(receiver_thread, NULL);
-  pthread_join(decoders[0], NULL);
-  pthread_join(decoders[1], NULL);
-  pthread_join(decoders[2], NULL);
-  pthread_join(decoders[3], NULL);
-
   printf("[*] Created decoders threads!\n");
+
+  if (pthread_create(&udp_sender, NULL, *udp_loop, NULL) < 0) {
+    perror("[!] Unable to create UDP thread.\n");
+    return 1;
+  }
+
+  printf("[*] Created UDP thread!\n");
+  printf("[*] Ready! Press CTRL + C to exit.\n");
+  while (keep_running)
+    pause();
+
+  system("clear");
+  printf("[!] Shutdown...\n");
+  buf_shutdown(ring_buffer);
+
+  pthread_join(udp_sender, NULL);
+  pthread_join(receiver_thread, NULL);
+  for (int i = 0; i < WORKERS; i++) {
+    pthread_join(decoders[i], NULL);
+  }
+
+  free(ring_buffer);
+  printf("Bye!\n");
 
   return 0;
 }
--- backend/src/receiver_can.c
+++ backend/src/receiver_can.c
@@ -1,6 +1,10 @@
 #include "receiver_can.h"
 #include "ring_buffer.h"
 
+#include <asm-generic/errno.h>
+#include <bits/types/struct_timeval.h>
+#include <errno.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +13,8 @@
 #include <linux/can.h>
 #include <pthread.h>
 #include <sys/socket.h>
+
+extern volatile sig_atomic_t keep_running;
 
 // Bind the socekt to the vcan0
 #include <net/if.h>
@@ -20,9 +26,14 @@ static int init_can_socket() {
   int fd = socket(AF_CAN, SOCK_RAW, CAN_RAW);
 
   if (fd < 0) {
-    perror("Socket not created; error occured.\n");
+    perror("[!] Can't create CAN socket!\n");
     exit(1);
   }
+
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 250;
+  setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
 
   unsigned int can_if_index = if_nametoindex("vcan0");
   if (can_if_index == 0) {
@@ -36,7 +47,7 @@ static int init_can_socket() {
   };
 
   if (bind(fd, (struct sockaddr *)&addr_can, sizeof(addr_can)) < 0) {
-    perror("[!] Can't bind the socket.\n");
+    perror("[!] Can't bind CAN socket.\n");
     exit(1);
   }
 
@@ -64,11 +75,13 @@ void *receiver_loop(void *b) {
   raw_frames_buffer *ring_buffer = (raw_frames_buffer *)b;
   int can_fd = init_can_socket();
 
-  while (1) {
+  while (keep_running) {
     struct can_frame cf;
     int bytes = get_can_frame(can_fd, &cf);
 
     if (bytes == -1) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
+        continue;
       perror("Read error.\n");
       continue;
     }
@@ -80,4 +93,6 @@ void *receiver_loop(void *b) {
     // Put frame into the buffer
     frame_put(ring_buffer, cf);
   }
+  close(can_fd);
+  return NULL;
 }
